bc09859a5dbff600bad0d251134a7a5f74ba2fcd
root <osmaoguzhan33@gmail.com>
2019-12-07 08:10:42 -0500

    First push

---
+++ deneme.py
@@ -0,0 +1,3 @@
+from netdiscover import *
+disc = Discover()
+disc.scan(ip_range="10.0.2.0/24")
---
+++ model/portScanner.py
@@ -0,0 +1,21 @@
+import nmap
+class portScanner():
+
+    def __init__(self, ip, port):
+        self.portScan = nmap.PortScanner()
+        self.getRange(ip, port)
+
+    def getRange(self, ip, port):
+        self.portScan.scan(ip, port)
+        self.scanPorts()
+
+    def scanPorts(self):
+        for host in self.portScan.all_hosts():
+            print('Host : %s (%s)' % (host, self.portScan[host].hostname()))
+            print('State : %s' % self.portScan[host].state())
+            for proto in self.portScan[host].all_protocols():
+                print('----------')
+                print('Protocol : %s' % proto)
+                lport = self.portScan[host][proto].keys()
+                for port in lport:
+                    print('port : %s\tstate : %s' % (port, self.portScan[host][proto][port]['state']))
