8aa5971dac3e8bd862f57a5d26d3c6a6178e9aa8
osmaoguzhan <osmaoguzhan33@gmail.com>
2020-01-12 06:00:38 -0500

    Table operations has been finished

--- akinci.py
+++ akinci.py
@@ -8,6 +8,7 @@
 
 from model import networkScanner
 import argparse
+from controller import dbOp as db
 
 __author__ = "Oguzhan Osma"
 __version__ = "v1.0"
@@ -16,10 +17,39 @@ __version__ = "v1.0"
 def argParse():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', "--port", required=False,
-                        help="Specific Port Ex: <-p 80>\nPort Range Ex: <-p 22-45>\nFor All Ports Ex <-p -1>\nAs a default option it'll scan all ports",
-                        default="-1")
+                        help="Specific Port Ex: <-p 80>\nPort Range Ex: <-p 22-45>\nFor All Ports Ex <-p -1>\n")
+    parser.add_argument('-d', "--db", required=False,
+                        help="1: MAC\n 2:IP Address\n 3: Host IP\n4: All")
     args = parser.parse_args()
-    networkScanner.networkScan(args.port)
+
+    if args.port != None:
+        networkScanner.networkScan(args.port)
+    elif args.db != None:
+        if args.db == "1":
+            mac = input("Please type the MAC you'd like to search on DB:")
+            if len(mac) > 0:
+                db.selectstar(str(mac),"mac")
+            else:
+                print("No written MAC!!")
+        elif args.db == "2":
+            ip = input("Please type the IP Address you'd like to search on DB:")
+            if len(ip) > 0:
+                db.selectstar(str(ip),"ip")
+            else:
+                print("Pls type an IP!!")
+        elif args.db == "3":
+            hostIP = input("Please type the Host IP you'd like to search on DB:")
+            if len(hostIP) > 0:
+                db.selectstar(str(hostIP), "hostip")
+            else:
+                print("No written Host IP!!")
+        elif args.db == "4":
+            db.getalldata()
+        else:
+            print("Wrong option!!")
+    else:
+        print("No arguments!!")
+
 
 
 def createBanner():
--- controller/dbOp.py
+++ controller/dbOp.py
@@ -2,15 +2,16 @@ import sqlite3
 from datetime import datetime
 from sqlite3 import Error
 import os
+import plotly.graph_objects
+
 
 def createTable():
     control = 1
-    if os.path.exists('info.db'):
+    if os.path.exists('dbFiles/info.db'):
         return control
     else:
         try:
-            print("VERİTABANI OLUŞTURULUYOR")
-            connection = sqlite3.connect('info.db')
+            connection = sqlite3.connect('dbFiles/info.db')
             cursor = connection.cursor()
             query = "Create table if not exists allInfo (" \
                     "hostip varchar (32)," \
@@ -33,7 +34,7 @@ def createTable():
 
 def insertinto(hostip, mac, ip, vendor, tcpPorts, udpPorts):
     if createTable() == 1 or createTable() == 0:
-        connection = sqlite3.connect('info.db')
+        connection = sqlite3.connect('dbFiles/info.db')
         cursor = connection.cursor()
         if len(tcpPorts) != 0:
             tcpPorts = ','.join([str(elem) for elem in tcpPorts])
@@ -55,31 +56,101 @@ def insertinto(hostip, mac, ip, vendor, tcpPorts, udpPorts):
     elif createTable() == -1:
         print("Got an error!!")
 
-def selectstar(mac):
-    connection = sqlite3.connect('info.db')
+def selectstar(data, argsOption):
+    info = []
     try:
-
+        connection = sqlite3.connect('dbFiles/info.db')
         cursor = connection.cursor()
-        query = "SELECT * FROM allInfo where mac = '" + str(mac) + "'"
+        query = "SELECT * FROM allInfo where " + str(argsOption) + "='" + str(data) + "'"
         info = cursor.execute(query).fetchall()
         connection.commit()
     except Error as e:
         print(e)
-        return False
+        info = False
+    finally:
+        connection.close()
+        plotTable(info)
 
-    return info
 
 def getalldata():
-    connection = sqlite3.connect('info.db')
-    cursor = connection.cursor()
-    query = "Select * from allInfo"
-    allData = cursor.execute(query).fetchall()
-    datas = []
-    for row in allData:
-        datas.append(row)
-    connection.commit()
-    connection.close()
-    return datas
+    try:
+        connection = sqlite3.connect('dbFiles/info.db')
+        cursor = connection.cursor()
+        query = "Select * from allInfo"
+        allData = cursor.execute(query).fetchall()
+        datas = []
+        for row in allData:
+            datas.append(row)
+        connection.commit()
+        connection.close()
+    except Error as e:
+        print(e)
+        datas = False
+    finally:
+        connection.close()
+        plotTable(datas)
 
-a=getalldata()
-print(a)
+def plotTable(datas):
+    hostIP = []
+    mac = []
+    ipAddr = []
+    macVendor= []
+    tcpPorts = []
+    udpPorts = []
+    date= []
+    if datas != False:
+        for i in range(len(datas)):
+            hostIP.append(datas[i][0])
+            mac.append(datas[i][1])
+            ipAddr.append(datas[i][2])
+            macVendor.append(datas[i][3])
+            tcpPorts.append(datas[i][4])
+            udpPorts.append(datas[i][5])
+            date.append(datas[i][6])
+        fig = plotly.graph_objects.Figure(data=[plotly.graph_objects.Table(
+            header=dict(values=['Host IP', 'IP Address', 'MAC' , 'MAC Vendor', 'TCP Ports', 'UDP Ports', 'Scan Date']),
+            cells=dict(values=[hostIP, ipAddr, mac, macVendor, tcpPorts, udpPorts, date]))],
+            layout_title_text="AKINCI is the tool for auto-scan local network")
+
+        fig.show()
+        checkPorts(tcpPorts)
+
+    else:
+        print("No data found!!")
+
+def checkPorts(tcpPorts):
+    listTcp= []
+    tcpPorts = ','.join([str(elem) for elem in tcpPorts if elem != ""])
+    if len(tcpPorts) != 0:
+        listTcp = tcpPorts.split(",")
+    listTcp = list(dict.fromkeys(listTcp))
+    info = []
+    ports = []
+    protocol = []
+    keywords = []
+    print(listTcp)
+    for i in range(len(listTcp)):
+        try:
+            connection = sqlite3.connect('dbFiles/ports.db')
+            cursor = connection.cursor()
+            query = "SELECT * FROM ports where port ='" + str(listTcp[i]) + "'"
+            info = cursor.execute(query).fetchall()
+            connection.commit()
+        except Error as e:
+            print(e)
+            info = False
+        finally:
+            connection.close()
+
+        for i in range(len(info)):
+            ports.append(info[i][0])
+            protocol.append(info[i][1])
+            keywords.append(info[i][2])
+
+
+    fig2 = plotly.graph_objects.Figure(data=[plotly.graph_objects.Table(
+        header=dict(values=['Ports', 'Protocol', 'Keyword']),
+        cells=dict(values=[ports, protocol, keywords]))],
+        layout_title_text="Listing of Port and Protocol, with known attacks.")
+    fig2.show()
+
